b64466457df736cf8e7d7b528b2733c028ea280b
antoinefa <antoinefa>
2017-01-31 15:54:51 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_2016_geohisto_communes_for_fuzzy

--- recipes/compute_2016_geohisto_communes_for_fuzzy.py
+++ recipes/compute_2016_geohisto_communes_for_fuzzy.py
@@ -5,11 +5,14 @@ from dataiku import pandasutils as pdu
 
 # Recipe inputs
 ds = dataiku.Dataset("2016_geohisto_communes_prep")
-df = ds.get_dataframe()
+df = ds.get_dataframe() #impossible ici d'ajouter un infer_pandas=False car l'une des colonnes est de type entier avec des NA
+#du coup le "dep_code" est pris par l'infer de pandas comme un entier, ce qui droppe les 2A 2B
+
 df = df[['norm_name','id']]
 
-
 df=df.groupby('norm_name').agg(lambda x: '[{}]'.format(','.join(x.astype(str)))).reset_index()
+
+#le département est recalculé par rapport à la commune via regexp
 df['dep_code']=df['id'].replace("COM(97.|..)...?\@....\-..\-..",r"\1", regex=True)
 
 # Recipe outputs
