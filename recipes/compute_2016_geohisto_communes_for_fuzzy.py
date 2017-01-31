3436875e6d8965297dc2786fa71e310f49ee9654
antoinefa <antoinefa>
2017-01-31 14:02:30 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_2016_geohisto_communes_for_fuzzy

--- recipes/compute_2016_geohisto_communes_for_fuzzy.py
+++ recipes/compute_2016_geohisto_communes_for_fuzzy.py
@@ -10,5 +10,5 @@ df = ds.get_dataframe(infer_with_pandas=False)
 df=df.groupby('nomr_name').agg(lambda x: '[{}]'.format(','.join(set(x))) )
 
 # Recipe outputs
-2016_geohisto_communes_for_fuzzy = dataiku.Dataset("2016_geohisto_communes_for_fuzzy")
-2016_geohisto_communes_for_fuzzy.write_with_schema(df)
+ds_out = dataiku.Dataset("2016_geohisto_communes_for_fuzzy")
+ds_out.write_with_schema(df)
