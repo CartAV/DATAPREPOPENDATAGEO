49b7ca99724910b09198e150c732032e73395f33
grabenertr <grabenertr>
2017-02-28 17:19:48 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_IGN_DEPARTEMENT_prep

--- recipes/compute_IGN_DEPARTEMENT_prep.sql
+++ recipes/compute_IGN_DEPARTEMENT_prep.sql
@@ -1,2 +1,4 @@
-SELECT *
+SELECT *,
+  st_transform(st_setSRID(st_makepoint("X_CHF_LIEU", "Y_CHF_LIEU"), 2154), 4346) as chef_lieu,
+  st_setSRID(st_geomfromtext(the_geom), 4326) as geometry
   FROM "ign_departement_pg"
