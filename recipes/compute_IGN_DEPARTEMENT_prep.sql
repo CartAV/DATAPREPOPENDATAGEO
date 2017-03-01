d8eed3a75b991bf2ac442f2cdf2d735a0c4a0442
grabenertr <grabenertr>
2017-03-01 18:37:08 +0100

    Deleted dataset DATAPREPOPENDATAGEO.IGN_DEPARTEMENT_prep

--- recipes/compute_IGN_DEPARTEMENT_prep.sql
+++
@@ -1,4 +0,0 @@
-SELECT *,
-  st_transform(st_setSRID(st_makepoint("X_CHF_LIEU", "Y_CHF_LIEU"), 2154), 4326) as chef_lieu,
-  st_setSRID(st_geomfromtext(the_geom), 4326) as geometry
-  FROM "ign_departement_pg"
