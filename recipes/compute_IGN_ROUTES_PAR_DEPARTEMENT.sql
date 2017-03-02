358a47dbba4fc9d3747ebaa3a0a8681b17d70d3e
grabenertr <grabenertr>
2017-03-02 18:07:13 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_IGN_ROUTES_PAR_DEPARTEMENT

--- recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT.sql
+++ recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT.sql
@@ -1,2 +1,4 @@
-SELECT *
-  FROM "ign_troncon_route_postgis"
+SELECT st_union(route.the_geom) as the_geom, dep.CODE_DEPT
+  FROM "ign_troncon_route_postgis" as route, "IGN_DEPARTEMENT_pg" as dep
+  WHERE st_within(route.the_geom, dep.the_geom)
+  GROUP BY dep.CODE_DEPT
