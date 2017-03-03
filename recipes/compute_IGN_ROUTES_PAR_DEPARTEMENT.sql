5159f2e62024b3131a39bcc258e9bce48e2cfce7
grabenertr <grabenertr>
2017-03-03 11:52:45 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_IGN_ROUTES_PAR_DEPARTEMENT

--- recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT.sql
+++ recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT.sql
@@ -1,4 +1,7 @@
-SELECT st_union(route.the_geom::geometry) as the_geom, dep."CODE_DEPT", "NUM_ROUTE"
+SELECT
+    st_union(route.the_geom::geometry) as the_geom,
+    dep."CODE_DEPT",
+    COALLESCE("NUM_ROUTE", "ID_RTE500") as num_route_or_id
   FROM "ign_troncon_route_postgis" as route, IGN_DEPARTEMENT_pg as dep
   WHERE st_within(route.the_geom::geometry, dep.the_geom::geometry)
-  GROUP BY dep."CODE_DEPT", "NUM_ROUTE"
+  GROUP BY dep."CODE_DEPT", num_route_or_id
