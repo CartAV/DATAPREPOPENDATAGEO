e530a91b59462b6ab17fd021c6a8f74027360524
grabenertr <grabenertr>
2017-03-06 11:34:57 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_ign_routes_par_commune

--- recipes/compute_ign_routes_par_commune.sql
+++ recipes/compute_ign_routes_par_commune.sql
@@ -4,6 +4,6 @@ SELECT
   route.num_route_or_id
 FROM
   ign_routes_par_departement as route,
-  ign_commune_postgis as commune
+  ign_commune_france as commune
 WHERE
   st_intersects(commune."the_geom", route."the_geom")
