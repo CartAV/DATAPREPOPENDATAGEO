860748c16031f6f4113ede9b57e542f2d078bb8e
grabenertr <grabenertr>
2017-03-09 16:24:01 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_IGN_ROUTES_PAR_DEPARTEMENT_1

--- recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT_1.sql
+++ recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT_1.sql
@@ -1,5 +1,5 @@
 SELECT
-    st_union(route.the_geom::geometry) as the_geom,
+    st_union(route.geom_way::geometry) as the_geom,
     dep."CODE_DEPT",
     COALESCE("osm_name", "id") as num_route_or_id
   FROM osm_2po_4pgr as route, IGN_DEPARTEMENT_FRANCE as dep
