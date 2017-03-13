a7b6e696ea6deacad66c16263f3cd5e94b06ec43
grabenertr <grabenertr>
2017-03-13 16:44:20 +0100

    Saved recipe: DATAPREPOPENDATAGEO.compute_ign_routes_par_commune_1

--- recipes/compute_ign_routes_par_commune_1.sql
+++ recipes/compute_ign_routes_par_commune_1.sql
@@ -1,6 +1,6 @@
 SELECT
   st_linemerge(st_intersection(commune."the_geom", route."the_geom")::geometry) as the_geom,
-  "CODE_COM",
+  "INSEE_COM",
   route.num_route_or_id
 FROM
   osm_routes_par_departement as route,
