a020ca1667d91098ab0574eab58ffa4cac3a3973
grabenertr <grabenertr>
2017-04-10 11:38:54 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_departements_geom

--- recipes/compute_departements_geom.sql
+++ recipes/compute_departements_geom.sql
@@ -1,3 +1,3 @@
-SELECT *
-  FROM "departements_wgs84_postgis" as dep, "osm_routes_par_departement" as routes
+SELECT dep.*, sum("cummul_km") as longueur_routes
+  FROM "IGN_DEPARTEMENT_FRANCE" as dep, "osm_routes_par_departement" as routes
   GROUP BY dep(CODE_DEPT)
