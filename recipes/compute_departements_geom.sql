bc2ff0bd7b6b8ffd4c7e683a2f787cf82ba23795
grabenertr <grabenertr>
2017-04-10 11:47:03 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_departements_geom

--- recipes/compute_departements_geom.sql
+++ recipes/compute_departements_geom.sql
@@ -1,3 +1,15 @@
-SELECT dep.*, sum("cummul_km") as longueur_routes
-  FROM "IGN_DEPARTEMENT_FRANCE" as dep, "osm_routes_par_departement" as routes
-  GROUP BY dep(CODE_DEPT)
+SELECT "CODE_REG",
+       "NOM_REG",
+       "CODE_DEPT",
+       "NOM_DEPT",
+       "CODE_CHF",
+       "NOM_CHF",
+       st_asgeojson(the_geom) as geojson,
+       longueur_routes
+  FROM "ign_departement_france" as dep
+  LATERAL (
+      SELECT sum("cummul_km") as longueur_routes
+        FROM "osm_routes_par_departement" as routes
+        WHERE dep.CODE_DEPT = routes.CODE_DEPT 
+  ) as routes
+  
