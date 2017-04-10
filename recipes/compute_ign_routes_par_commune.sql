42f9d95e3b9184ad18e67fbca7f99c7bdc72c35f
grabenertr <grabenertr>
2017-04-10 10:04:04 +0200

    Deleted dataset DATAPREPOPENDATAGEO.ign_routes_par_commune

--- recipes/compute_ign_routes_par_commune.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  st_linemerge(st_intersection(commune."the_geom", route."the_geom")::geometry) as the_geom,
-  route."CODE_DEPT",
-  route.num_route_or_id
-FROM
-  ign_routes_par_departement as route,
-  ign_commune_france as commune
-WHERE
-  st_intersects(commune."the_geom", route."the_geom")
