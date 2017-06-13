db7ec67e81262c24526a814ff1da4e421e86971e
grabenertr <grabenertr>
2017-06-13 14:08:35 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_IGN_ROUTES_PAR_DEPARTEMENT_1

--- recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT_1.sql
+++ recipes/compute_IGN_ROUTES_PAR_DEPARTEMENT_1.sql
@@ -10,6 +10,6 @@ SELECT
         END
     ) as cummul_km
         
-  FROM osm_2po_4pgr as route, IGN_DEPARTEMENT_FRANCE as dep
+  FROM osm_france_dom as route, IGN_DEPARTEMENT_FRANCE as dep
   WHERE st_within(route.geom_way::geometry, dep.the_geom::geometry)
   GROUP BY dep."CODE_DEPT", num_route_or_id, route_dep_id
