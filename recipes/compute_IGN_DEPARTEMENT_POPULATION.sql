6d0b27b9f8425551f9ef84d82c6363c1de303a22
grabenertr <grabenertr>
2017-06-23 16:52:25 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_IGN_DEPARTEMENT_POPULATION

--- recipes/compute_IGN_DEPARTEMENT_POPULATION.sql
+++ recipes/compute_IGN_DEPARTEMENT_POPULATION.sql
@@ -1,6 +1,6 @@
 SELECT dep.*,
-       sum(commune.population) as population
+       sum(commune."POPULATION") as population
   FROM "ign_departement_france" as dep
   JOIN "ign_commune_france" as commune
-    ON commune."CODE_DEPT" = dept."CODE_DEPT"
+    ON commune."CODE_DEPT" = dep."CODE_DEPT"
   GROUP BY dep."CODE_DEPT"
