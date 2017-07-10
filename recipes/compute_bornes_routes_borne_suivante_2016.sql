096a317b5e74291a0d7c801559a81719d6bac31c
chabouisfr <chabouisfr>
2017-07-10 16:15:12 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_bornes_routes_borne_suivante_2016

--- recipes/compute_bornes_routes_borne_suivante_2016.sql
+++ recipes/compute_bornes_routes_borne_suivante_2016.sql
@@ -2,7 +2,7 @@ with b1 as (select * from "public"."bornes_routes_communes_2016")
 select b1.lon as lon_next, b1.lat as lat_next, st_distance(ST_SetSRID(ST_MakePoint(b1.lon, b1.lat),4326)::geography, ST_SetSRID(ST_MakePoint(b2.lon, b2.lat),4326)::geography) as distance_next, b2.* 
 from "public"."bornes_routes_communes_2016" b2 left outer join b1
 ON b1.route = b2.route and b1.pr = b2.pr + 1 and b1.cote = b2.cote and st_distance(ST_SetSRID(ST_MakePoint(b1.lon, b1.lat),4326)::geography, ST_SetSRID(ST_MakePoint(b2.lon, b2.lat),4326)::geography)<1600
-order by route, pr, concession , distance
+order by route, pr, concession , distance_next
 
 /* Pour chaque borne, trouve la borne sur la meme route, dont le pr vaut pr+1, du même côté, qui est à moins de 1600m */
 /* Sinon, on laisse le champs vide */
