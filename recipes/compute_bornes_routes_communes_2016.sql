7b225165cffe4b593313089552ebb3fe8ad1d389
chabouisfr <chabouisfr>
2017-07-10 12:13:01 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_bornes_routes_communes_2016

--- recipes/compute_bornes_routes_communes_2016.sql
+++ recipes/compute_bornes_routes_communes_2016.sql
@@ -1,2 +1,2 @@
-SELECT *
-  FROM "bornes_routes_2016"
+select c."INSEE_COM", c."NOM_COM", b.* from "public"."ign_commune_france" c, "public"."bornes_routes_2016" b 
+where ST_CONTAINS(c.the_geom::geometry, ST_SetSRID(ST_MakePoint(b.lon, b.lat),4326))
