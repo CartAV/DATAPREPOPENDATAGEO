925870b98767bd72ee9d48f4731d854b9923e8e0
chabouisfr <chabouisfr>
2017-07-19 11:46:53 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_bornes_routes_borne_suivante_2016

--- recipes/compute_bornes_routes_borne_suivante_2016.sql
+++ recipes/compute_bornes_routes_borne_suivante_2016.sql
@@ -8,3 +8,9 @@ order by route, pr, concession , distance_next
 /* Sinon, on laisse le champs vide */
 /* Pas de check sur le departement, car la borne suivante peut être dans un autre departement */
 /* Pas de check sur la concession car parfois même si le concessionnaire change, il y a continuité des pr */
+
+with b1 as (select * from "public"."bornes_routes_communes_2016") 
+select b1.lon as lon_next2, b1.lat as lat_next2, st_distance(ST_SetSRID(ST_MakePoint(b1.lon, b1.lat),4326)::geography, ST_SetSRID(ST_MakePoint(b2.lon, b2.lat),4326)::geography) as distance_next, b2.* 
+from "public"."bornes_routes_communes_2016" b2 left outer join b1
+ON b1.route = b2.route and b1.pr = b2.pr + 2 and st_distance(ST_SetSRID(ST_MakePoint(b1.lon, b1.lat),4326)::geography, ST_SetSRID(ST_MakePoint(b2.lon, b2.lat),4326)::geography)<1600
+order by route, pr, concession , distance_next
