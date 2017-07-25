357e8c1b8e56f97711995952c2468b8be24d5802
chabouisfr <chabouisfr>
2017-07-25 15:25:09 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_bornes_routes_milieu_2016

--- recipes/compute_bornes_routes_milieu_2016.sql
+++ recipes/compute_bornes_routes_milieu_2016.sql
@@ -1,4 +1,4 @@
 SELECT route, nom_courant_route, pr, "depPr", concession, gestionnai, avg(lat) as lat, avg(lon) as lon
-  FROM "bornes_routes_2016" group by route, nom_courant_route, pr, "depPr", concession, gestionnai order by route, pr
+  FROM "bornes_routes_2016_stacked" group by route, nom_courant_route, pr, "depPr", concession, gestionnai order by route, pr
 
 /*on garde un point unique qui est la moyenne des pk droits et gauches"*/
