7fc394a4e0643a491bdcd99502cac98f23a7313d
chabouisfr <chabouisfr>
2017-07-27 14:56:50 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_osm_routes_cas_particuliers

--- recipes/compute_osm_routes_cas_particuliers.sql
+++ recipes/compute_osm_routes_cas_particuliers.sql
@@ -1,2 +1,5 @@
 SELECT *
   FROM "osm_routes_par_departement"
+  WHERE ("CODE_DEPT" = '75' AND "num_route_or_id" like '%Boulevard Périphérique%' AND "cat_route_osm" = 'route principale')
+
+/* Traitement a part du periph parisien pour ne pas qu'il se fasse saucissonner */
