2199b76bf33b35e21eb55b52cd3dcca430e1f48b
chabouisfr <chabouisfr>
2017-07-28 09:50:09 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_osm_routes_cas_particuliers

--- recipes/compute_osm_routes_cas_particuliers.sql
+++ recipes/compute_osm_routes_cas_particuliers.sql
@@ -1,4 +1,4 @@
-SELECT ST_UNION(the_geom::geometry) as the_geom, 'Boulevard Périphérique75' as num_route_com_id, '75056' as "INSEE_COM", 'Boulevard Périphérique' as num_route_or_id, 'route principale' as cat_route_osm
+SELECT ST_UNION(the_geom::geometry)::text as the_geom, 'Boulevard Périphérique75' as num_route_com_id, '75056' as "INSEE_COM", 'Boulevard Périphérique' as num_route_or_id, 'route principale' as cat_route_osm
   FROM "osm_routes_par_departement"
   WHERE ("CODE_DEPT" = '75' AND "num_route_or_id" like '%Boulevard Périphérique%' AND "cat_route_osm" = 'route principale')
 
