af8fc77b4184cef8a8e219ad73a1a508fd5c5420
grabenertr <grabenertr>
2017-10-27 11:31:37 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_osm_routes_par_departement_general

--- recipes/compute_osm_routes_par_departement_general.sql
+++ recipes/compute_osm_routes_par_departement_general.sql
@@ -1,5 +1,5 @@
 -- On traite le périph parisien séparément pour unifier l’intérieur et extérieur
-SELECT the_geom,  "CODE_DEP", num_route_or_id, cat_route_osm
+SELECT the_geom,  "CODE_DEPT", num_route_or_id, cat_route_osm
   FROM "osm_routes_par_departement"
   WHERE NOT ("CODE_DEPT" = '75' AND "num_route_or_id" like '%Boulevard Périphérique%' AND "cat_route_osm" = 'route principale')
 
