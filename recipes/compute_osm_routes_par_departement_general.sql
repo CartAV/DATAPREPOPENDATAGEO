8e145e8ddb7a712124cd2ff90bfd8720f7ddf830
grabenertr <grabenertr>
2017-10-27 11:31:48 +0200

    Saved recipe: DATAPREPOPENDATAGEO.compute_osm_routes_par_departement_general

--- recipes/compute_osm_routes_par_departement_general.sql
+++ recipes/compute_osm_routes_par_departement_general.sql
@@ -5,6 +5,6 @@ SELECT the_geom,  "CODE_DEPT", num_route_or_id, cat_route_osm
 
 UNION
 
-SELECT ST_UNION(the_geom::geometry) as the_geom, '75056' as "INSEE_COM", 'Boulevard Périphérique' as num_route_or_id, 'route principale' as cat_route_osm
+SELECT ST_UNION(the_geom::geometry) as the_geom, '75' as "CODE_DEPT", 'Boulevard Périphérique' as num_route_or_id, 'route principale' as cat_route_osm
   FROM "osm_routes_par_departement"
   WHERE ("CODE_DEPT" = '75' AND "num_route_or_id" like '%Boulevard Périphérique%' AND "cat_route_osm" = 'route principale')
